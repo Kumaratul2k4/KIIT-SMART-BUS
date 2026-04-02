70c11d1658c69b635624bcfecd06f6ffb832c6d4
Kumaratul2k4 <2229196@kiit.ac.in>
2026-04-02 14:21:38 +0530

    moved firebase config to env and updated gitignore

--- src/utils/db.js
+++ src/utils/db.js
@@ -28,14 +28,14 @@ import {
 } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDVcc0-uhxSFFgXUBW1Fjh8F_CFWuoAzGA",
-  authDomain: "kiit-smartbus-ff329.firebaseapp.com",
-  databaseURL: "https://kiit-smartbus-ff329-default-rtdb.firebaseio.com",
-  projectId: "kiit-smartbus-ff329",
-  storageBucket: "kiit-smartbus-ff329.firebasestorage.app",
-  messagingSenderId: "479430905856",
-  appId: "1:479430905856:web:174f7688249147256a08e5",
-  measurementId: "G-TC6JW02DM0",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const app       = initializeApp(firebaseConfig);
