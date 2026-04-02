2cf3e1a503a76be0603e8ed2c04be356ba6fc06e
Atul kumar <2229196@kiit.ac.in>
2026-04-02 17:06:46 +0530

    Log Firebase configuration variables
    Added console logs for Firebase configuration variables.

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,4 +1,12 @@
 import { initializeApp } from "firebase/app";
+console.log("API KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
+console.log("AUTH DOMAIN:", import.meta.env.VITE_FIREBASE_AUTH_DOMAIN);
+console.log("DATABASE URL:", import.meta.env.VITE_FIREBASE_DATABASE_URL);
+console.log("PROJECT ID:", import.meta.env.VITE_FIREBASE_PROJECT_ID);
+console.log("STORAGE BUCKET:", import.meta.env.VITE_FIREBASE_STORAGE_BUCKET);
+console.log("MESSAGING SENDER ID:", import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID);
+console.log("APP ID:", import.meta.env.VITE_FIREBASE_APP_ID);
+console.log("MEASUREMENT ID:", import.meta.env.VITE_FIREBASE_MEASUREMENT_ID); 
 import {
   getAuth,
   createUserWithEmailAndPassword,
